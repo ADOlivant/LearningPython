385787ceac41de7f26db87f6211549bc8dd4c6d0
Adam David Olivant <adolivant@gmail.com>
2015-06-28 22:43:21 +0100

    Python School OOP Task 3
    Converted direct access to methods within the class to be called when
    required.

--- OOP_Object_Orientated_Programming/crop_class.py
+++ OOP_Object_Orientated_Programming/crop_class.py
@@ -16,13 +16,24 @@ class Crop:
 		#the above attributes are prefixed with an underscore to indicate 
 		#that they should not be accessed directly from outwith the class
 
+	#method to inidcate the needs of the crop 
+	def needs(self): 
+		#return a dictionary containing the light and water needs
+		return {'light need': self._light_need, 'water need': self._water_need}
+
+	#method to report the provided information about the current state of the 
+	#crop
+	def report(self): 
+		#return a dictionary containing the type, growth, and days growing
+		return {'type': self._type, 'status':self._status, 'growth':self._growth, 'days growing':self._days_growing}
+
+
 def main(): 
 	#instaniate the class
 	new_crop = Crop(1,4,3)
 	#test to see wheether it works or not
-	print(new_crop._status)
-	print(new_crop._light_need)
-	print(new_crop._water_need)
+	print(new_crop.needs())
+	print(new_crop.report())
 
 if __name__ == "__main__":
 	main()

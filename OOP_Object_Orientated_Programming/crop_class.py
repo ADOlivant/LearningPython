c2e620ef1be03f9bee58e6a9bd6178ff561de79d
Adam David Olivant <adolivant@gmail.com>
2015-06-28 22:31:45 +0100

    Python School OOP Task 2
    Tested the Crop class.

--- OOP_Object_Orientated_Programming/crop_class.py
+++ OOP_Object_Orientated_Programming/crop_class.py
@@ -4,5 +4,25 @@ class Crop:
 	#constructor
 	def __init__(self, growth_rate, light_need, water_need):
 		#set the attributes with initial value
+
+		self._growth = 0
+		self._days_growing = 0
+		self._growth_rate = growth_rate
+		self._light_need = light_need
+		self._water_need = water_need
+		self._status = "Seed"
+		self._type = "Generic"
 		
-		
+		#the above attributes are prefixed with an underscore to indicate 
+		#that they should not be accessed directly from outwith the class
+
+def main(): 
+	#instaniate the class
+	new_crop = Crop(1,4,3)
+	#test to see wheether it works or not
+	print(new_crop._status)
+	print(new_crop._light_need)
+	print(new_crop._water_need)
+
+if __name__ == "__main__":
+	main()

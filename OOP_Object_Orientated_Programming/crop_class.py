29dec966105f63735c1f0f10ed711ca29a4c8890
Adam David Olivant <adolivant@gmail.com>
2015-06-28 22:55:40 +0100

    Python School OOP Task 4
    Added in grow method with update status.

--- OOP_Object_Orientated_Programming/crop_class.py
+++ OOP_Object_Orientated_Programming/crop_class.py
@@ -27,12 +27,37 @@ class Crop:
 		#return a dictionary containing the type, growth, and days growing
 		return {'type': self._type, 'status':self._status, 'growth':self._growth, 'days growing':self._days_growing}
 
+	#the underscore indicates that this method hould not be called from outwith the class
+	def _update_status(self):
+		#update the status of the crop based on the growth
+		if self._growth > 15: 
+			self._status = "Old"
+		elif self._growth > 10: 
+			self._status = "Mature"
+		elif self._growth > 5: 
+			self._status = "Young"
+		elif self._growth > 0: 
+			self._status = "Seedling"
+		elif self._growth == 0: 
+			self._status = "Seed"
+
+	def grow(self,light,water): 
+		#grow the crop dependant on the light and water available and need and to 
+		#update the required attributes if completed.
+		if light >= self._light_need and water >= self._water_need: 
+			self._growth += self._growth_rate
+		#increment days growing
+		self._days_growing += 1
+		#update the status
+		self._update_status()
 
 def main(): 
 	#instaniate the class
 	new_crop = Crop(1,4,3)
 	#test to see wheether it works or not
 	print(new_crop.needs())
+	print(new_crop.report())
+	new_crop.grow(4,4)
 	print(new_crop.report())
 
 if __name__ == "__main__":
